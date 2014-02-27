a14c5d46f48d09da10bb840bd6726e6137c1a6f7
Gilles Charvin <gilles.charvin@gmail.com>
2014-02-27 17:55:32 +0100

    add link cell nucleus

---
+++ at_linkCellNucleus.m
@@ -0,0 +1,49 @@
+function arr=at_linkCellNucleus
+global segmentation timeLapse
+
+arr=-1*ones(length(segmentation.tcells1),timeLapse.numberOfFrames); % nuclei index in tcells object
+
+%nuclei at interface : % nucleus number , cell list,  frame
+% interface=[];
+% interface.nucleus=[];
+% interface.cell=[];
+% interface.frame=[];
+% 
+% ifcount=1;
+
+
+for i=1:length(segmentation.tcells1)
+    fprintf('.');
+    frames=[segmentation.tcells1(i).Obj.image];
+    arr(i,frames)=0;
+    
+    cc=1;
+    for j=frames
+        nucleus=segmentation.nucleus(j,:);
+        cells1= segmentation.tcells1(i).Obj(cc);
+        
+        xc=cells1.x;
+        yc=cells1.y;
+        
+        n=[]; score=[];
+        for k=1:length(nucleus)
+            xn=nucleus(k).x;
+            yn=nucleus(k).y;
+            
+            scorek=mean(inpolygon(xn,yn,xc,yc));
+            
+            if scorek>0.2 % nucleus is partly inside the cell
+                n=[n nucleus(k).n];
+                score=[score scorek];
+            end 
+        end
+        if numel(n)
+        [score pix]=sort(score,'descend'); % sort nuclei according to fraction of contour found in cell
+        n=n(pix);
+        arr(i,j)=n(1);
+        end
+        cc=cc+1;
+    end
+end
+
+fprintf('\n');
