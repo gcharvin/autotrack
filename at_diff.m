2822eaf803d63103bc98e037d8c5911c5e25479c
Gilles Charvin <gilles.charvin@gmail.com>
2014-03-31 10:57:42 +0200

    changes to at_diff.m

--- at_diff.m
+++ at_diff.m
@@ -19,31 +19,58 @@ display=[display1 display2 display3 display4];
 
 % now plot differrences nicely using traj
 
-colM=colorpic;
+% colM=colorpic;
+% 
+% z=size(colM,1);
+% rm=1:1:z*z;
+% rm=reshape(rm,[z z]);
 
-z=size(colM,1);
-rm=1:1:z*z;
-rm=reshape(rm,[z z]);
+thr=0.3;
 
-thr=0.5;
+% colind=reshape(colM,[size(colM,1)*size(colM,1) 1 3]);
+% colind=permute(colind,[1 3 2]);
+% 
+% colind=1-colormap(jet(z));
 
-colind=reshape(colM,[size(colM,1)*size(colM,1) 1 3]);
-colind=permute(colind,[1 3 2]);
+s=0.1;
+
+r1=[1:-s:0 zeros(1,length(s:s:1))];
+g1=[zeros(1,length(1:-s:0)) s:s:1];
+b1=zeros(size(g1));
+
+colind=[r1' g1' b1'];
+colind=flipud(colind);
+z=size(colind,1);
 
 M={};
 
-h=figure('Color','w','Position',[200 200 1000 500]);
+h=figure('Color','w','Position',[200 200 1200 500]);
 cellwidth=20; spacing=1.2;
-startX=0;
-startY=0;
+
 
 p=panel;
-p.pack('v',{1/2 []});
+p.pack('h',{0.05 0.85 0.05 0.05});
 
-p.fontsize=12;
+p.fontsize=16;
+
+p(2).pack('v',{1/2 1/2});
+p(1).pack('v',{1/2 1/2});
 
 for ii=0:1
-    p(ii+1).select();
+    
+    hlabel=p(1,ii+1).select();
+    set(hlabel,'Visible','off');
+    if ii==0
+       text(0.5,0.5,'D','FontSize',20); 
+    else
+       text(0.5,0.5,'M','FontSize',20); 
+    end
+    
+    
+    h_axis=p(2,ii+1).select();
+    
+    startX=0;
+    startY=0;
     
 for j=1:length(statarr)
     stats=datastat(statarr(j)).stats;
@@ -60,32 +87,42 @@ for j=1:length(statarr)
     CV{j}=std(T_M,0,1)./avg{j};
     
     
-    
     r1=(avg{j}./avg{1}-1);
     r1=min(r1,thr); r1=max(r1,-thr); 
     
     r1=max(1,round(z*0.5*(r1+thr)./thr));
-    r1=z*ones(1,length(r1));
+    %r1=z*ones(1,length(r1));
     
     r2=(CV{j}./CV{1}-1); r2=min(r2,thr); r2=max(r2,-thr); r2=max(1,round(z*0.5*(r2+thr)./thr));
-    r2=z*ones(1,length(r2));
+    %r2=1*ones(1,length(r2));
     
     rec=[]; cindex=[];
     for i=1:length(avg{j})
+       
+            
+            
         rec(i,1)=cellwidth*(i-1);
         rec(i,2)=cellwidth*i;
         
         if j==1
-           cindex(i) =1; 
+           cindex(i)=(z+1)/2; 
         else
             %size(rm),r1(i),r2(i)
-           id=sub2ind(size(rm),r1(i),r2(i))
-           cindex(i)= id;
+           %id=sub2ind(size(rm),r1(i),r2(i))
+           cindex(i)= r1(i);
         end
+        
+         if i==11 
+             cindex(i)=(z+1)/2; 
+         end
+          if i==12 
+             cindex(i)=(z+1)/2; 
+          end
+         
     end
     startY=startY+spacing*cellwidth;
     %if j==1
-        Traj(rec,'Color',colind,'colorindex',cindex,'tag',['Cell type:' strarr{j} '-' num2str(ii)],h,'width',cellwidth,'startX',startX,'startY',startY,'sepColor',[1 1 1],'sepwidth',1,'gradientWidth',0);
+        Traj(rec,'Color',colind,'colorindex',cindex,'tag',['Cell type:' strarr{j} '-' num2str(ii)],h,'width',cellwidth,'startX',startX,'startY',startY,'sepColor',[1 1 1],'sepwidth',2,'gradientWidth',0);
     %end
 end
 
@@ -97,9 +134,31 @@ axis equal tight
 
 end
 
+colormap(colind);
+h_colorbar_axis = colorbar('peer', h_axis);
+
+lab={};
+for i=1:11;
+   lab{i}=100*thr*(i-6)/(11-6); 
+end
+
+set(h_colorbar_axis,'YTick',0:0.1:1,'YTickLabel',lab);
+ylabel(h_colorbar_axis,'Variations compared to WT (%)  ');
+
+p(3).select(h_colorbar_axis);
+
+p.de.margin=2;
+%p(3).margin=[1 1 1 1]
+
+
+f2=[path '/' file '-compare-synth.pdf'];    
+
+myExportFig(f2,'-zbuffer');
+
+
 function out=colorpic
 
-s=0.1;
+s=0.25;
 
 r1=[1:-s:0 zeros(1,length(s:s:1))];
 g1=[zeros(1,length(1:-s:0)) s:s:1];
@@ -129,6 +188,6 @@ for i=1:size(m,1)
     end
 end
 
-figure, imshow(m);
+%figure, imshow(m);
 
 out=m;
