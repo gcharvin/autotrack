5f38e36c78c027adc9d71f6f46a62d9ffaf7f07e
Gilles Charvin <gilles.charvin@gmail.com>
2014-04-02 13:51:21 +0200

    fixed corrupted file issue when saving on server

--- at_save.m
+++ at_save.m
@@ -5,5 +5,13 @@ pos=segmentation.position;
 
 fprintf(['Saving Position: ' num2str(pos) '...\n']);
     
-save(fullfile(timeLapse.realPath,timeLapse.pathList.position{pos},'segmentation-autotrack.mat'),'segmentation');
-save(fullfile(timeLapse.realPath,[timeLapse.filename '-project.mat']),'timeLapse');
+localpath=userpath;
+localpath=localpath(1:end-1);
+
+save([localpath '/segmentation-autotrack.mat'],'segmentation');
+copyfile([localpath '/segmentation-autotrack.mat'],fullfile(timeLapse.realPath,timeLapse.pathList.position{pos},'segmentation-autotrack.mat'));
+%save(fullfile(timeLapse.realPath,timeLapse.pathList.position{pos},'segmentation-autotrack.mat'),'segmentation');
+
+save([localpath '/timeLapse.mat'],'timeLapse');
+copyfile([localpath '/timeLapse.mat'],fullfile(timeLapse.realPath,[timeLapse.filename '-project.mat']));
+
